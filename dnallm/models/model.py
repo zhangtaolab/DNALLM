9abc64e6bd9db5a1c4a5c6ea96af66e8c2357174
ljq12697 <928623130@qq.com>
2025-10-13 12:44:27 +0800

    mypy error fixed

--- dnallm/models/model.py
+++ dnallm/models/model.py
@@ -1063,10 +1063,7 @@ def _handle_megadna_models(
                 def get_vocab(self) -> dict[str, int]:
                     return self.token_to_id.copy()
 
-                def _tokenize(self,
-                              text: Any,
-                              **kwargs: Any
-                              ) -> list[str]:  # type: ignore[override]
+                def _tokenize(self, text: Any, **kwargs: Any) -> list[str]:
                     return list(text)
 
                 def _convert_token_to_id(self, token: str) -> int:
